34738d81acc0a9a198c2ab3737104c5863df26ac
Miles Egan <miles@plastaq.com>
2021-08-02 10:42:08 -0600

    Add simple geometry.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,59 @@
-import { Canvas, useFrame } from "@react-three/fiber";
-import React, { useRef, useState } from "react";
-import * as THREE from "three";
+import { Html, Line, QuadraticBezierLine, useAspect } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React, { useState } from "react";
 
-function Box(props: JSX.IntrinsicElements["mesh"]) {
-  const mesh = useRef<THREE.Mesh>(null!);
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+function Paths() {
+  const [isOver, setIsOver] = useState(false);
+  const scale = useAspect(window.innerWidth, window.innerHeight);
+  console.log({ scale });
+
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
+    <>
+      <mesh scale={[scale[1], scale[1], 1]}>
+        <Line
+          points={[
+            [0, 0, 0],
+            [0.5, 0.25, 0],
+            [0.375, 0, 0],
+          ]}
+          color="red"
+          lineWidth={1}
+        />
+        <QuadraticBezierLine
+          start={[0, 0, 0]} // Starting point
+          end={[0.5, 0.5, 0]} // Ending point
+          mid={[0.5, 0, 0]} // Optional control point
+          color="white" // Default
+          lineWidth={1} // In pixels (default)
+          dashed={false} // Default
+        />
+      </mesh>
+      <mesh
+        scale={[scale[1], scale[1], 1]}
+        position={[200, 0, 0]}
+        onPointerDown={() => setIsOver(!isOver)}
+        // onPointerEnter={() => setIsOver(true)}
+        // onPointerLeave={() => setIsOver(false)}
+      >
+        <boxGeometry args={[0.25, 0.25, -1]} />
+        <meshBasicMaterial color={isOver ? "red" : "gray"} attach="material" />
+        <Html style={{ userSelect: "none", pointerEvents: "none" }}>
+          <h1>Hello</h1>
+        </Html>
+      </mesh>
+    </>
   );
 }
 
 export function App() {
   return (
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+    <Canvas
+      gl={{ alpha: false }}
+      frameloop="demand"
+      orthographic={true}
+      dpr={window.devicePixelRatio}
+    >
+      <Paths />
     </Canvas>
   );
 }
