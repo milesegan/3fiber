88a493594e98b5f529022d81ba2411c3d4df0eeb
Miles Egan <miles@plastaq.com>
2021-08-03 08:49:23 -0600

    Fat line drawing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,37 @@
-import { Html, Line, QuadraticBezierLine, useAspect } from "@react-three/drei";
+import { Html, Line as DreiLine, useAspect } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
+import { Path, Vector3 } from "three";
 
 function Paths() {
   const [isOver, setIsOver] = useState(false);
+  const [lineOver, setLineOver] = useState(false);
+  const points = useMemo(() => {
+    const p = new Path();
+    p.quadraticCurveTo(0.375, 0, 0.5, 0.5);
+    return p.getPoints(20).map((p) => new Vector3(p.x, p.y, 0));
+  }, []);
   const scale = useAspect(window.innerWidth, window.innerHeight);
-  console.log({ scale });
 
   return (
-    <>
-      <mesh scale={[scale[1], scale[1], 1]}>
-        <Line
-          points={[
-            [0, 0, 0],
-            [0.5, 0.25, 0],
-            [0.375, 0, 0],
-          ]}
-          color="red"
-          lineWidth={1}
-        />
-        <QuadraticBezierLine
-          start={[0, 0, 0]} // Starting point
-          end={[0.5, 0.5, 0]} // Ending point
-          mid={[0.5, 0, 0]} // Optional control point
-          color="white" // Default
-          lineWidth={1} // In pixels (default)
-          dashed={false} // Default
+    <group scale={[scale[1], scale[1], 1]}>
+      <mesh>
+        <DreiLine
+          points={points}
+          color={lineOver ? "red" : "white"}
+          lineWidth={10}
+          onPointerEnter={() => setLineOver(true)}
+          onPointerLeave={() => setLineOver(false)}
         />
       </mesh>
-      <mesh
-        scale={[scale[1], scale[1], 1]}
-        position={[200, 0, 0]}
-        onPointerDown={() => setIsOver(!isOver)}
-        // onPointerEnter={() => setIsOver(true)}
-        // onPointerLeave={() => setIsOver(false)}
-      >
+      <mesh position={[0.25, 0, 0]} onPointerDown={() => setIsOver(!isOver)}>
         <boxGeometry args={[0.25, 0.25, -1]} />
         <meshBasicMaterial color={isOver ? "red" : "gray"} attach="material" />
         <Html style={{ userSelect: "none", pointerEvents: "none" }}>
           <h1>Hello</h1>
         </Html>
       </mesh>
-    </>
+    </group>
   );
 }
 
