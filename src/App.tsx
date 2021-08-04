66322c2ca660c32d3ea0553f4c7f6920946dca84
Miles Egan <miles@plastaq.com>
2021-08-04 16:44:38 -0600

    Cleanup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,43 @@
-import { useSpring, a } from "@react-spring/three";
 import { Canvas, useThree } from "@react-three/fiber";
 import { useGesture } from "react-use-gesture";
-import React from "react";
+import React, { useState } from "react";
 
 function Boxes() {
   const { size, viewport } = useThree();
   const aspect = size.width / viewport.width;
-  const [spring, set] = useSpring(() => ({
+  const [state, set] = useState({
     scale: [1, 1, 1],
     position: [0, 0, 0],
     rotation: [0, 0, 0],
     config: { friction: 10 },
-  }));
+  });
 
   const bind = useGesture({
     onDrag: ({ event, offset: [x, y] }) => {
       event.stopPropagation();
       if (event.shiftKey) {
-        set.start({ position: [x / aspect, -y / aspect, 0] });
+        set({ ...state, rotation: [y / aspect, x / aspect, 0] });
       } else {
-        set.start({ rotation: [y / aspect, x / aspect, 0] });
+        set({ ...state, position: [x / aspect, -y / aspect, 0] });
       }
     },
     onHover: ({ hovering }) =>
-      set.start({ scale: hovering ? [1.2, 1.2, 1.2] : [1, 1, 1] }),
+      set({ ...state, scale: hovering ? [1.2, 1.2, 1.2] : [1, 1, 1] }),
   });
 
   return (
-    <a.mesh position={[1, 1, 1]} {...(spring as any)} {...(bind() as any)}>
-      <dodecahedronBufferGeometry args={[1, 0]} />
-      <meshNormalMaterial />
-    </a.mesh>
+    <mesh {...state} {...(bind() as any)}>
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
   );
 }
 
 export function App() {
   return (
     <Canvas>
-      <ambientLight intensity={0.5} />
+      <color attach="background" args={["black"]} />
+      <ambientLight intensity={0.1} />
       <spotLight
         intensity={0.6}
         position={[20, 10, 10]}
